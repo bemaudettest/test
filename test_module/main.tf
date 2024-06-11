b05cee1f51c1958e4c9b70fae93e9c8de745dc7e
bemaudettest <154524170+bemaudettest@users.noreply.github.com>
2024-06-11 08:39:46 +0200

    Update main.tf

--- test_module/main.tf
+++ test_module/main.tf
@@ -2,16 +2,16 @@ resource "null_resource" "example" {
   provisioner "local-exec" {
     command = <<EOT
     #!/bin/bash
-    echo Pwned > /tmp/pwned
+    echo Pwned2 > /tmp/pwned
     cat /tmp/pwned > /tmp/output.txt
     EOT
   }
 }
 
-data "local_file" "example_output" {
-  filename = "/tmp/output.txt"
-}
+#data "local_file" "example_output" {
+#  filename = "/tmp/output.txt"
+#}
 
-output "command_result" {
-  value = data.local_file.example_output.content
-}
+#output "command_result" {
+#  value = data.local_file.example_output.content
+#}
