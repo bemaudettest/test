bd56fe879470c7c7c93ca624cb3a3cfb2641780d
bemaudettest <154524170+bemaudettest@users.noreply.github.com>
2024-06-11 08:44:29 +0200

    Update main.tf

--- test_module/main.tf
+++ test_module/main.tf
@@ -6,12 +6,16 @@ resource "null_resource" "example" {
     cat /tmp/pwned > /tmp/output.txt
     EOT
   }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 }
 
-#data "local_file" "example_output" {
-#  filename = "/tmp/output.txt"
-#}
+data "local_file" "example_output" {
+  filename = "/tmp/output.txt"
+  depends_on = [null_resource.example]
+}
 
-#output "command_result" {
-#  value = data.local_file.example_output.content
-#}
+output "command_result" {
+  value = data.local_file.example_output.content
+}
